9b33578596c31e62beabbc5e16a2c2ce26b347e0
Nikita-ПК\Nikita <sen-deni.nikita@yandex.ru>
2018-04-05 13:15:11 +0400

    Исправления

--- js/script.js
+++ js/script.js
@@ -5,15 +5,46 @@
    var form = popup.querySelector("form");
    var login = popup.querySelector("[name=feedback-user-name]");
    var email = popup.querySelector("[name=feedback-user-email]");
+   var isStorageSupport = true;
+  var storage = "";
+  
+  try {
+    storage = localStorage.getItem("login");
+  } catch (err) {
+    isStorageSupport = false;
+  }
+
    link.addEventListener("click", function (evt) {
+    evt.preventDefault();
     popup.classList.add("modal-show");
-    login.focus();
+    if (storage) {
+      login.value = storage;
+      email.focus();
+    } else {
+      login.focus();
+    }
   });
    close.addEventListener("click", function (evt) {
-    evt.preventDefault();
     popup.classList.remove("modal-show");
   });
    form.addEventListener("submit", function (evt) {
-    evt.preventDefault();
+    if (!login.value || !email.value) {
+      evt.preventDefault();
+      console.log("Нужно ввести логин и пароль");
+    }else {
+      if (isStorageSupport) {
+        localStorage.setItem("login", login.value);
+      }
+    }
   });
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("modal-show")) {
+        popup.classList.remove("modal-show");
+      }
+    }
+  });
+
 })();
