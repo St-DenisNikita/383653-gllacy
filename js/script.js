744b9faf94cc0981102439174398a2c3fcd492d5
Nikita-ПК\Nikita <sen-deni.nikita@yandex.ru>
2018-04-06 12:16:43 +0400

    Финальные изменения

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 (function () {
    var link = document.querySelector(".feedback-button");
    var popup = document.querySelector(".feedback-popup");
+   var overlay = document.querySelector(".popup-overlay");
    var close = popup.querySelector(".close-button");
    var form = popup.querySelector("form");
    var login = popup.querySelector("[name=feedback-user-name]");
    var email = popup.querySelector("[name=feedback-user-email]");
+   var text = popup.querySelector("[name=feedback-form-text]");
    var isStorageSupport = true;
-  var storage = "";
+   var loginStoraged = "";
+   var emailStoraged = "";
   
   try {
-    storage = localStorage.getItem("login");
+    loginStoraged = localStorage.getItem("login");
+    emailStoraged = localStorage.getItem("email");
   } catch (err) {
     isStorageSupport = false;
   }
@@ -17,21 +21,34 @@
    link.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add("modal-show");
-    if (storage) {
-      login.value = storage;
-      email.focus();
-    } else {
+    overlay.classList.add("modal-show-overlay");
+
+    if (loginStoraged && emailStoraged) {
+      login.value = loginStoraged;
+      email.value = emailStoraged;
+      text.focus();
+    } else if(emailStoraged && !loginStoraged){
+      email.value = emailStoraged;
       login.focus();
+    } else if(!emailStoraged && loginStoraged){
+      login.value = loginStoraged;
+      email.focus();
     }
   });
+
    close.addEventListener("click", function (evt) {
     popup.classList.remove("modal-show");
+    overlay.classList.remove("modal-show-overlay");
+    popup.classList.remove("modal-error");
   });
+
    form.addEventListener("submit", function (evt) {
-    if (!login.value || !email.value) {
+    if (!login.value || !email.value || !text.value) {
       evt.preventDefault();
-      console.log("Нужно ввести логин и пароль");
-    }else {
+      popup.classList.remove("modal-error");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("modal-error");
+    } else {
       if (isStorageSupport) {
         localStorage.setItem("login", login.value);
       }
@@ -43,6 +60,7 @@ window.addEventListener("keydown", function (evt) {
       evt.preventDefault();
       if (popup.classList.contains("modal-show")) {
         popup.classList.remove("modal-show");
+        popup.classList.remove("modal-error");
       }
     }
   });
